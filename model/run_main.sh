9ec43a6b37ab666cbf5984b4e909c9161ca01ba6
piosj <yeppyteen2@naver.com>
2025-07-25 15:27:34 +0900

    .

--- model/run_main.sh
+++ model/run_main.sh
@@ -5,12 +5,12 @@ GPU_NUM=0
 HOP=2
 INTERVAL_MINUTES=30
 BATCH_SIZE=300
-METHOD='cnn_attention'   # 'cnn_attention''multihead_self_attention'
-NO_CATEGORY=False
+METHOD='multihead_self_attention'   # 'cnn_attention''multihead_self_attention'
+NO_CATEGORY=True
 UNIQUE_CATEGORY=False
 ADJUST_SCORE=True
 
-for SEED in 2025 1024 42 256; do # 64 2025 1024 42 256
+for SEED in 2025; do # 64 2025 1024 42 256
     echo ">>> Running with SEED=$SEED, BATCH_SIZE=$BATCH_SIZE, GPU_NUM=$GPU_NUM, HOP=$HOP, INTERVAL_MINUTES=$INTERVAL_MINUTES, METHOD=$METHOD, NO_CATEGORY=$NO_CATEGORY, UNIQUE_CATEGORY=$UNIQUE_CATEGORY, ADJUST_SCORE=$ADJUST_SCORE"
     
     env \
